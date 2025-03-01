5e120cafed84ba624799443dc36b60992176e7bf
tibs <tibs@tibinonest.me>
2025-03-01 10:32:16 -0500

    Don't use ninja pls

--- build.sh
+++ build.sh
@@ -2,10 +2,8 @@
 cd "$(dirname "$0")"
 cmake \
   -S . \
-  -G Ninja \
   -B cmake-build-debug \
   -DCMAKE_BUILD_TYPE=Debug \
-  -DCMAKE_MAKE_PROGRAM=ninja \
   -DCMAKE_C_COMPILER=clang \
   -DCMAKE_CXX_COMPILER=clang++ \
 
