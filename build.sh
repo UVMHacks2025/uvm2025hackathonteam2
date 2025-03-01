b12c6ef8199e3dcca02fbe6fcf6f172ce9a49e48
tibs <tibs@tibinonest.me>
2025-03-01 10:33:34 -0500

    Change build dir

--- build.sh
+++ build.sh
@@ -2,10 +2,15 @@
 cd "$(dirname "$0")"
 cmake \
   -S . \
-  -B cmake-build-debug \
+  -B build \
   -DCMAKE_BUILD_TYPE=Debug \
   -DCMAKE_C_COMPILER=clang \
   -DCMAKE_CXX_COMPILER=clang++ \
 
-cd cmake-build-debug
+cd build
 cmake --build .
+
+
+if [[ $1 = "run" ]]; then
+  exec ./hack
+fi
