066f1a2034abd0bd126d5d81b4c1c7bc23e7abad
tibs <tibs@tibinonest.me>
2025-03-01 10:49:32 -0500

    More cleanup

--- flake.nix
+++ flake.nix
@@ -9,20 +9,14 @@
           pkgs = nixpkgs.legacyPackages.x86_64-linux;
         in
         {
-          default = pkgs.mkShell rec {
-            buildInputs = with pkgs; [
-
-            ];
+          default = pkgs.mkShell {
             nativeBuildInputs = with pkgs; [
-              # Tooling
               cmake
               extra-cmake-modules
-              libgcc
               pkg-config
               rocmPackages.llvm.clang
               rocmPackages.llvm.libunwind
             ];
-            LD_LIBRARY_PATH = pkgs.lib.makeLibraryPath buildInputs;
           };
         };
     };
