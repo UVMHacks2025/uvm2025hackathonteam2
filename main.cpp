d6f5246ceac739b6d7433c02490f33b39eed171e
Ellie <ejgookin@gmail.com>
2025-03-01 10:29:52 -0500

    Fixed testingAtticus and testingEllie

--- main.cpp
+++ main.cpp
@@ -1,5 +1,7 @@
 #include "interface.h"
 #include <iostream>
+
+#include "question.h"
 using namespace std;
 //mac was here
 
--- testingEllie.cpp
+++ testingEllie.cpp
@@ -1,3 +1,7 @@
 //
 // Created by Ellie Gookin on 3/1/25.
 //
+
+int main() {
+  return 0;
+}
